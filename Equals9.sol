938adfa3d60dd46b90e557e4b990883c6f860437
bernardo9999 <bernardo.madeira1@gmail.com>
2021-08-03 21:06:53 -0300

    final smart contract

--- Equals9.sol
+++ Equals9.sol
@@ -10,11 +10,11 @@ import "./ERC20Capped.sol";
 import "./Pausable.sol";
 
 /**
- * @title Equals9 Token (EQ9)
- * @author Bernardo Madeira
- * @notice Implements a basic ERC20 staking token with incentive distribution.
+ * @title equals9 (EQ9)
+ * @author Equals9 Empreendimentos e Participações S/A
+ * @notice Implements a basic ERC20 utility & staking token.
  */
-contract StakingToken is ERC20, ERC20Mintable, ERC20Capped, ERC20Detailed, ERC20Burnable, Pausable, Ownable {
+contract EQ9UtilityToken is ERC20, ERC20Mintable, ERC20Capped, ERC20Detailed, ERC20Burnable, Pausable, Ownable {
     using SafeMath for uint256;
 
     /**
@@ -37,7 +37,7 @@ contract StakingToken is ERC20, ERC20Mintable, ERC20Capped, ERC20Detailed, ERC20
      * @param _owner The address to receive all tokens on construction.
      * @param _supply The amount of tokens to mint on construction.
      */
-    constructor (address _owner, uint256 _supply) ERC20Capped(10000000000000000000) ERC20Detailed('Equals9 ERC-20 Token', 'EQ9', 8)
+    constructor (address _owner, uint256 _supply) ERC20Capped(180000000000000000) ERC20Detailed('equals9', 'EQ9', 8)
         public
     { 
      _mint(_owner, _supply * (10 ** uint256(decimals())));
@@ -140,72 +140,5 @@ contract StakingToken is ERC20, ERC20Mintable, ERC20Capped, ERC20Detailed, ERC20
             stakeholders[s] = stakeholders[stakeholders.length - 1];
             stakeholders.pop();
         } 
-    }
-
-    // ---------- REWARDS ----------
-    
-    /**
-     * @notice A method to allow a stakeholder to check his rewards.
-     * @param _stakeholder The stakeholder to check rewards for.
-     */
-    function rewardOf(address _stakeholder) 
-        public
-        view
-        returns(uint256)
-    {
-        return rewards[_stakeholder];
-    }
-
-    /**
-     * @notice A method to the aggregated rewards from all stakeholders.
-     * @return uint256 The aggregated rewards from all stakeholders.
-     */
-    function totalRewards()
-        public
-        view
-        returns(uint256)
-    {
-        uint256 _totalRewards = 0;
-        for (uint256 s = 0; s < stakeholders.length; s += 1){
-            _totalRewards = _totalRewards.add(rewards[stakeholders[s]]);
-        }
-        return _totalRewards;
-    }
-
-    /** 
-     * @notice A simple method that calculates the rewards for each stakeholder.
-     * @param _stakeholder The stakeholder to calculate rewards for.
-     */
-    function calculateReward(address _stakeholder)
-        public
-        view
-        returns(uint256)
-    {
-        return stakes[_stakeholder] / 100;
-    }
-
-    /**
-     * @notice A method to distribute rewards to all stakeholders.
-     */
-    function distributeRewards() 
-        public
-        onlyOwner
-    {
-        for (uint256 s = 0; s < stakeholders.length; s += 1){
-            address stakeholder = stakeholders[s];
-            uint256 reward = calculateReward(stakeholder);
-            rewards[stakeholder] = rewards[stakeholder].add(reward);
-        }
-    }
-
-    /**
-     * @notice A method to allow a stakeholder to withdraw his rewards.
-     */
-    function withdrawReward() 
-        public
-    {
-        uint256 reward = rewards[msg.sender];
-        rewards[msg.sender] = 0;
-        _mint(msg.sender, reward);
     }
 }
